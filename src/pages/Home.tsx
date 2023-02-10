6006855c8a82e755d6b723458843a9225c441344
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-02-10 16:28:45 -0300

    avanços com redux

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
+import { charactersList } from "../store/characters";
 import { attributes } from '../store/slices/slices';
 function Home() {
   const dispatch = useDispatch();
-  const characters = useSelector((state : any) => state.characters)
+  const characters = useSelector((state : any) => state.characters);
+  useEffect(() => {
+    dispatch(charactersList());
+  }), [dispatch];
   return (
     <>
-      {  }
+      { characters }
     </>
   );
 }
--- src/store/characters.ts
+++ src/store/characters.ts
@@ -3,17 +3,12 @@ import { apiService } from '../service/request';
 import { perso } from '../types/types'
 export const charactersList = createAsyncThunk(
     "", async () => {
-        let response: perso[] = []
-        while (response.length < 8) {
-            await apiService.get("/characters")
-            .then((res : any) => {
-                /* !variavel && response.push(res); */
-                /* return res */
-            })
+        const response: perso[] =
+            await apiService.get("/characters/random?limit=8")
+            .then((res : any) => {console.log(res); return res })
             .catch((err : any) => {
                 console.log(err);
             });
-        }
         return response;
     }
 );
